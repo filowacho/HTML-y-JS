5818fc68d3b240c32b25232d3cc4edd4abde6bda
Den Sartori <chino.dookie@hotmail.com>
2019-10-31 20:51:29 -0300

    Acá hay un HTML con un JS
    Mi primer HTML y JS

---
+++ codigo.js
@@ -0,0 +1,7 @@
+alert("Si funciona!!");
+var ruta = window.location;
+var version= navigator.appVersion;
+console.log(ruta);
+console.log(navigator);
+document.write ("Estás en: " + ruta);
+document.write ("<br/> </br> Estás en: " + version );
